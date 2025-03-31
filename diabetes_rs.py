fd0ccd896b1092f95a750001dafdee7c6cded487
Khai Nguyen <khainguyen2004@gmail.com>
2025-03-30 22:47:05 -0700

    Updated diabetes data

--- diabetes_rs.py
+++ diabetes_rs.py
@@ -1,8 +1,12 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+
 from sklearn.preprocessing import StandardScaler
+
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
 from sklearn.metrics import classification_report
 
@@ -26,7 +30,7 @@ data = pd.read_csv("diabetes.csv")
 # print(data.corr())
 
 target = "Outcome"
-# Drop the Outcome column
+# Drop the Outcome column, axis = 1 is column, axis = 0 is row
 x = data.drop(target, axis = 1)
 
 # Obtain Outcome column
@@ -45,20 +49,42 @@ x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
 # Fit support vector machine model
-model = SVC()
-model.fit(x_train, y_train)
+# SVCmodel = SVC()
+# SVCmodel.fit(x_train, y_train)
+# y_predict = SVCmodel.predict(x_test)
 
-y_predict = model.predict(x_test)
-# for i, j in zip(y_predict, y_test):
-#     print(f"Prediction: {i}. Actual values: {j}")
+# Fit logistic regression model
+# LRmodel = LogisticRegression()
+# LRmodel.fit(x_train, y_train)
+# y_predict = LRmodel.predict(x_test)
 
+# Fit random forest model
+RDmodel = RandomForestClassifier()
+RDmodel.fit(x_train, y_train)
+y_predict = RDmodel.predict(x_test)
+
+
+print(type(y_predict))
+for i, j in zip(y_predict, y_test):
+    print(f"Prediction: {i}. Actual values: {j}")
+
+# threshold = 0.3
+#
+# for score in y_predict:
+#     if score[1] > threshold:
+#         print("Class 1")
+#     else:
+#         print("Class 0")
 
 # print(f"Acc: {accuracy_score(y_test, y_predict)}")
 # print(f"Precision: {precision_score(y_test, y_predict)}")
 # print(f"Recall: {recall_score(y_test, y_predict)}")
 # print(f"F1 score: {f1_score(y_test, y_predict)}")
 
-print(f"Acc: {classification_report(y_test, y_predict)}")
+# Use the classification report
+# We want to focus in recall for class 1 because we want to get as many patient with potential of cancer as we can
+
+print(f"{classification_report(y_test, y_predict)}")
 
 
 
